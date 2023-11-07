edb4132f49e61bcf8bc8f4573358d6cbde5b5db9
bouharri <bouharri@nmu.edu>
2023-11-07 12:26:19 -0500

    initial: server working

---
+++ astro.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from "astro/config"
+import node from "@astrojs/node"
+import svelte from "@astrojs/svelte"
+import tailwind from "@astrojs/tailwind"
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [
+        svelte(),
+        tailwind({
+            config: {
+                applyBaseStyles: false,
+            },
+        }),
+    ],
+    trailingSlash: "always",
+    site: "https://uplionsserve.org",
+    base: "/childhood-cancer",
+    output: "server",
+    adapter: node({
+        mode: "middleware",
+    }),
+    build: {
+        server: "../../web-server/src/childhood-cancer/server",
+        client: "../../web-server/src/childhood-cancer/client",
+    },
+})
