d858dc0580013e24cbba330cb98aa3b538a05da0
Harrison Bouche <harrison@sm.media>
2023-12-19 12:56:55 -0500

    update: new data on home map

--- src/components/home/OurProgress.svelte
+++ src/components/home/OurProgress.svelte
@@ -1,19 +1,21 @@
 <script lang="ts">
-  import Headline from "@components/_fragments/Headline.svelte";
+import Headline from "@components/_fragments/Headline.svelte"
+
+const { BASE_URL } = import.meta.env
 </script>
 
-<section class="w-full px-8 mb-20 flex flex-col items-center gap-8">
-  <Headline>Our <span class="text-orange">Progress</span></Headline>
-  <img
-    src="{import.meta.env.BASE_URL}/graphics/data/cc-data-family-map.svg"
-    alt="A map of the Upper Peninsula showcasing amount of recognized families per county."
-    class="max-w-lg lg:max-w-2xl xl:max-w-3xl 2xl:max-w-4xl"
-  />
-  <p class="text-center max-w-md">
-    These numbers represent the families we recognize who are fighting childhood
-    cancer. <span class="font-bold"
-      >Every dollar earned goes directly to the families and to the programs
-      that serve them.</span
-    >
-  </p>
+<section class="mb-20 flex w-full flex-col items-center gap-8 px-8">
+    <Headline>Our <span class="text-orange">Progress</span></Headline>
+    <img
+        src="{BASE_URL}/graphics/data/cc-data-family-map.svg"
+        alt="A map of the Upper Peninsula showcasing amount of recognized families per county."
+        class="max-w-lg lg:max-w-2xl xl:max-w-3xl 2xl:max-w-4xl"
+    />
+    <p class="max-w-md text-center">
+        These numbers represent the families we recognize who are fighting
+        childhood cancer. <span class="font-bold"
+            >Every dollar earned goes directly to the families and to the
+            programs that serve them.</span
+        >
+    </p>
 </section>
