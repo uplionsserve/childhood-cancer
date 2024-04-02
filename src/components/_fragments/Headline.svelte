b2c3eef53b0014b2771895ea19d50bbf2a967625
Harrison Bouche <harrison@sm.media>
2024-04-02 14:40:32 -0400

    fix: capitalize photo category titles (temp)

--- src/components/_fragments/Headline.svelte
+++ src/components/_fragments/Headline.svelte
@@ -1,41 +1,46 @@
 <script lang="ts">
-  export let direction: string = "center";
-  export let inline: boolean = false;
-  export let id: string | null = null;
+export let direction: string = "center"
+export let inline: boolean = false
+export let id: string | null = null
+export let capitalize: boolean = false
 
-  switch (direction) {
+switch (direction) {
     case "left":
-      direction = "items-start";
-      break;
+        direction = "items-start"
+        break
     case "center":
-      direction = "items-center";
-      break;
+        direction = "items-center"
+        break
     case "right":
-      direction = "items-end";
-      break;
-  }
+        direction = "items-end"
+        break
+}
 </script>
 
-<div class="{inline ? 'w-auto' : 'w-full'} flex flex-col {direction}">
-  <h2 {id} class="text-3xl scroll-mt-36 md:scroll-mt-32">
-    <slot>Section <span class="text-orange">Headline</span></slot>
-  </h2>
-  <div
-    class="
-            py-2 space-y-2
-            flex flex-col {direction}"
-  >
-    <hr
-      class="
-                w-40 h-0.5
-                bg-orange rounded-full
-                border-none"
-    />
-    <hr
-      class="
-                w-24 h-0.5
-                bg-orange-light rounded-full
-                border-none"
-    />
-  </div>
+<div
+    class="{inline
+        ? 'w-auto'
+        : 'w-full'} flex flex-col {direction} {capitalize && 'capitalize'}"
+>
+    <h2 {id} class="scroll-mt-36 text-3xl md:scroll-mt-32">
+        <slot>Section <span class="text-orange">Headline</span></slot>
+    </h2>
+    <div
+        class="
+            flex flex-col
+            space-y-2 py-2 {direction}"
+    >
+        <hr
+            class="
+                h-0.5 w-40
+                rounded-full border-none
+                bg-orange"
+        />
+        <hr
+            class="
+                h-0.5 w-24
+                rounded-full border-none
+                bg-orange-light"
+        />
+    </div>
 </div>
